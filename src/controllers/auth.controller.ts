1e8161d8af4418b20d60f66d2bc6f1742264ba72
RezaR2P <kecilreza575@gmail.com>
2026-07-30 12:16:10 +0700

    feat: membuat endpoint register dengan validasi menggunakan yup

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,45 @@
+import { Request, Response } from 'express';
+import * as Yup from 'yup';
+type TRegister = {
+  fullName: string;
+  userName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
+const registerValidateSchema = Yup.object({
+  fullName: Yup.string().required(),
+  userName: Yup.string().required(),
+  email: Yup.string().required(),
+  password: Yup.string().required(),
+  confirmPassword: Yup.string()
+    .required()
+    .oneOf([Yup.ref('password'), ''], 'Password Not Match'),
+});
+
+export default {
+  async register(req: Request, res: Response) {
+    const { fullName, userName, email, password, confirmPassword } =
+      req.body as unknown as TRegister;
+    try {
+      await registerValidateSchema.validate({
+        fullName,
+        userName,
+        email,
+        password,
+        confirmPassword,
+      });
+      res.status(200).json({
+        message: 'Success Registration',
+        data: { fullName, userName, email },
+      });
+    } catch (error) {
+      const err = error as unknown as Error;
+      res.status(400).json({
+        message: err.message,
+        data: null,
+      });
+    }
+  },
+};
